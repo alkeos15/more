93622dcaec93e8282158e1a2fcd7c44a9a65a733
alkeos <alkeos.stelios@gmail.com>
2024-11-13 18:40:56 +0000

    Fix tests

---
+++ frontend/src/App.test.js
@@ -0,0 +1,13 @@
+// src/App.test.js
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders frontend service and backend message', () => {
+  render(<App />);
+  const titleElement = screen.getByText(/Frontend Service/i);
+  expect(titleElement).toBeInTheDocument();
+
+  const messageElement = screen.getByText(/Message from Backend:/i);
+  expect(messageElement).toBeInTheDocument();
+});
+
